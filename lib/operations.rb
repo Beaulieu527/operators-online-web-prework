8c550ca8bc228e416fa43e931635d29a917053d8
beaulieudev <beaulieudev@gmail.com>
2018-10-06 20:58:56 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,5 +9,5 @@ end
 
 
 def not_safe?(speed)
-	speed ? speed < 40 || speed > 60 "Going 30 mph on the freeway would be unsafe, as would going 95 mph. " :"This is safe!"
-end
+	 speed < 40 || speed > 60 ? true : false
+ end
